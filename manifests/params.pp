d1130b827bf0cf4b3087b4395a353824155e80aa
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-03-03 14:02:37 +1300

    = not => for assignment

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,7 @@
 class puppetdashboard::params {
-  $app_dir    => '/usr/share/puppet/puppet-dashboard'
-  $user       => 'puppet-dashboard'
-  $user_home  => "/home/${user}"
-  $group      => $user
-  $git_source => 'git://github.com/puppetlabs/puppet-dashboard.git'
+  $app_dir    = '/usr/share/puppet/puppet-dashboard'
+  $user       = 'puppet-dashboard'
+  $user_home  = "/home/${user}"
+  $group      = $user
+  $git_source = 'git://github.com/puppetlabs/puppet-dashboard.git'
 }
